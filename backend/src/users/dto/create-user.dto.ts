b9ac26e2d85fff5d7a4d1cbc9b244def6606b10b
zrafat80 <zrafat80@gmail.com>
2024-12-08 20:33:05 +0200

    estabilishing users Module

---
+++ backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,23 @@
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  phone_number: string;
+
+  @IsString()
+  gender: string;
+
+  @IsNotEmpty()
+  role_id: number;
+}
