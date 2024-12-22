31761cc442bf2f1e77c3760d5b1597df84db5292
Ammar_Khaled <ammarkhaled2003@gmail.com>
2024-12-22 09:35:12 +0200

    update branch.entity.ts path

--- backend/src/branches/entities/branch.entity.ts
+++ backend/src/branches/entities/branch.entity.ts
@@ -7,22 +7,38 @@ import {
   OneToMany,
 } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
-import { Address } from './address.entity';
+import { Address } from '../../common/entities/address.entity';
+import {
+  IsString,
+  IsNotEmpty,
+  IsBoolean,
+  IsOptional,
+  IsPhoneNumber,
+} from 'class-validator';
 
 @Entity('branches')
 export class Branch {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsString()
+  @IsNotEmpty()
   @Column({ type: 'varchar', length: 255, nullable: false })
   name: string;
 
+  @IsString()
+  @IsOptional()
   @Column({ type: 'text', nullable: true })
   description: string;
 
+  @IsString()
+  @IsOptional()
+  @IsPhoneNumber()
   @Column({ type: 'varchar', length: 20, nullable: true, unique: true })
   phone: string;
 
+  @IsBoolean()
+  @IsOptional()
   @Column({ type: 'boolean', default: true })
   isActive: boolean;
 
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -7,7 +7,7 @@ import {
 } from 'typeorm';
 import { Address } from '../../common/entities/address.entity';
 import { Role } from '../../roles/role.entity';
-import { Branch } from '../../common/entities/branch.entity';
+import { Branch } from '../../branches/entities/branch.entity';
 
 @Entity('users')
 export class User {
