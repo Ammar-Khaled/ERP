ec8f4f13d905c1e9a2b2b5bb3241a697640e6ac0
Ammar_Khaled <ammarkhaled2003@gmail.com>
2024-12-23 18:12:53 +0200

    create address when create branch

--- backend/src/branches/branches.module.ts
+++ backend/src/branches/branches.module.ts
@@ -4,12 +4,14 @@ import { BranchesService } from './branches.service';
 import { DataSource } from 'typeorm';
 import { Branch } from './entities/branch.entity';
 import { DatabaseModule } from '../common/database.module';
+import { addressProviders } from '../common/address.providers';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [BranchesController],
   providers: [
     BranchesService,
+    ...addressProviders,
     {
       provide: 'BRANCH_REPOSITORY',
       useFactory: (dataSource: DataSource) => dataSource.getRepository(Branch),
--- backend/src/branches/branches.service.ts
+++ backend/src/branches/branches.service.ts
@@ -1,12 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Branch } from './entities/branch.entity';
+import { CreateBranchDto } from './dto/create-branch.dto';
+import { Address } from '../common/entities/address.entity';
 
 @Injectable()
 export class BranchesService {
   constructor(
     @Inject('BRANCH_REPOSITORY')
     private readonly branchRepository: Repository<Branch>,
+    @Inject('ADDRESS_REPOSITORY')
+    private readonly addressRepository: Repository<Address>,
   ) {}
 
   async findAll(): Promise<Branch[]> {
@@ -17,8 +21,17 @@ export class BranchesService {
     return this.branchRepository.findOne({ where: { id } });
   }
 
-  async create(branch: Branch): Promise<Branch> {
-    return this.branchRepository.save(branch);
+  async create(createBranchDto: CreateBranchDto): Promise<Branch> {
+    // check if branch has adderss
+    if (createBranchDto.address) {
+      // create address
+      const address = await this.addressRepository.create(
+        createBranchDto.address,
+      );
+      await this.addressRepository.save(address);
+      createBranchDto.address = address;
+    }
+    return this.branchRepository.save(createBranchDto);
   }
 
   async update(id: number, updatedBranch: Branch): Promise<Branch> {
