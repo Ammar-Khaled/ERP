c81bb08705f24f1f0a659cd3225d7d45319be510
Ammar_Khaled <ammarkhaled2003@gmail.com>
2024-12-25 14:46:26 +0200

    Refactor Clients module to integrate database and improve logic
    Replaced in-memory logic with TypeORM for persistent storage, added related database providers, and refactored entity relationships. Enhanced data validation, error handling, and CRUD operations to align with database integration. Removed outdated tests and updated the module setup to support new dependencies.

--- backend/src/clients/entities/client.entity.ts
+++ backend/src/clients/entities/client.entity.ts
@@ -1,1 +1,27 @@
-export class Client {}
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Address } from 'src/common/entities/address.entity';
+
+@Entity('clients')
+export class Client {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  phone_number: string;
+
+  @OneToOne(() => Address, { cascade: false, eager: true }) // The 'address' relation
+  @JoinColumn({ name: 'address_id' }) // This links the foreign key 'address_id' to the Address table
+  address?: Address; // Make address optional as well, in case the address_id is not provided
+}
