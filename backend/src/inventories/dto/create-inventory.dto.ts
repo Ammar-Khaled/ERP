7c3371ddc2433cf6af2199f203876f838df6b1b5
Ammar_Khaled <ammarkhaled2003@gmail.com>
2024-12-27 16:40:18 +0200

    Add inventory module with CRUD operations and validations
    Introduced the inventories module, including its controller, service, DTOs, and entity for managing inventory records. Implemented CRUD operations with validation and integration with branches and addresses. Ensured unique inventory names and proper exception handling for missing branches.

---
+++ backend/src/inventories/dto/create-inventory.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { CreateAddressDto } from '../../common/dtos/create-address.dto';
+import { Type } from 'class-transformer';
+
+export class CreateInventoryDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive: boolean;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CreateAddressDto)
+  address?: CreateAddressDto;
+
+  @IsOptional()
+  @IsInt()
+  branchId?: number;
+}
