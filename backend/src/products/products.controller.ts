3c605f0b03e84ee9cf7aa7a50e5a8413ff54bde3
Ammar_Khaled <ammarkhaled2003@gmail.com>
2025-02-14 19:02:44 +0200

    remove the Public decorator

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -10,32 +10,31 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { Public } from 'src/auth/auth.guard';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
-  @Public()
+
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
-  @Public()
+
   @Get()
   findAll() {
     return this.productsService.findAll();
   }
-  @Public()
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
-  @Public()
+
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
-  @Public()
+
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
