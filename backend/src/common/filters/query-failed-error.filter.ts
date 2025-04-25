631fc2ee03d4593270a8e3a77dd9afba6ea1edf0
Ammar_Khaled <ammarkhaled2003@gmail.com>
2025-04-25 16:20:23 +0300

    add a new error

--- backend/src/common/filters/query-failed-error.filter.ts
+++ backend/src/common/filters/query-failed-error.filter.ts
@@ -62,14 +62,6 @@ export class QueryFailedErrorFilter implements ExceptionFilter {
           data: null,
         });
 
-      case 1146: // ER_NO_SUCH_TABLE
-        return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-          isSuccess: false,
-          message: 'Table does not exist: ' + sqlMessage,
-          data: null,
-        });
-
       case 1406: // ER_DATA_TOO_LONG
         return response.status(HttpStatus.BAD_REQUEST).json({
           statusCode: HttpStatus.BAD_REQUEST,
@@ -78,11 +70,11 @@ export class QueryFailedErrorFilter implements ExceptionFilter {
           data: null,
         });
 
-      case 1064: // ER_PARSE_ERROR
+      case 1364: // ER_NO_DEFAULT_FOR_FIELD
         return response.status(HttpStatus.BAD_REQUEST).json({
           statusCode: HttpStatus.BAD_REQUEST,
           isSuccess: false,
-          message: 'SQL syntax error: ' + sqlMessage,
+          message: 'Field does not have a default value: ' + sqlMessage,
           data: null,
         });
 
