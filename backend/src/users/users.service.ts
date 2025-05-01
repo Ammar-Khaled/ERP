f682640860343b0958b17e8a881a855f8a04dc13
Ammar_Khaled <ammarkhaled2003@gmail.com>
2025-05-01 18:39:09 +0300

    include DatabaseLoggerService in users module and service for enhanced logging

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -13,6 +13,7 @@ import { Role } from '../roles/entities/role.entity';
 import { Branch } from '../branches/entities/branch.entity';
 import { config } from 'dotenv';
 import * as process from 'node:process';
+import { DatabaseLoggerService } from '../logging/database-logger.service';
 
 config();
 
@@ -22,6 +23,8 @@ export class UsersService {
     @Inject('USER_REPOSITORY') private userRepository: Repository<User>,
     @Inject('ROLE_REPOSITORY') private roleRepository: Repository<Role>,
     @Inject('BRANCH_REPOSITORY') private branchRepository: Repository<Branch>,
+    @Inject(DatabaseLoggerService)
+    private readonly databaseLogger: DatabaseLoggerService,
   ) {}
 
   async findAll() {
@@ -37,6 +40,9 @@ export class UsersService {
       );
       delete user.purchaseRequests;
     });
+
+    await this.databaseLogger.log('Fetched all users', UsersService.name);
+
     return users;
   }
 
