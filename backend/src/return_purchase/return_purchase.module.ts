970eaa36803f9def659f374fbcc29d105cc69ff5
Saeed Ahmed <saeed.developer18@gmail.com>
2025-06-26 19:03:04 +0300

    fix missing imports

--- backend/src/return_purchase/return_purchase.module.ts
+++ backend/src/return_purchase/return_purchase.module.ts
@@ -3,8 +3,10 @@ import { ReturnPurchaseService } from './return_purchase.service';
 import { ReturnPurchaseController } from './return_purchase.controller';
 import { returnPurchaseProviders } from './return_purchase.providers';
 import { returnPurchaseItemProviders } from './return_purchase_item.providers';
+import { DatabaseModule } from 'src/common/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ReturnPurchaseController],
   providers: [
     ReturnPurchaseService,
