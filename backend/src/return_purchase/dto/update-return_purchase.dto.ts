c171aad21db8dc874fb6ed21ec1692418ad31f68
Saeed Ahmed <saeed.developer18@gmail.com>
2025-06-27 11:39:51 +0300

    Implement update DTO for ReturnPurchase

--- backend/src/return_purchase/dto/update-return_purchase.dto.ts
+++ backend/src/return_purchase/dto/update-return_purchase.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateReturnPurchaseDto } from './create-return_purchase.dto';
 
-export class UpdateReturnPurchaseDto extends PartialType(CreateReturnPurchaseDto) {}
+export class UpdateReturnPurchaseDto extends PartialType(
+    // Can't update the purchaseRequestId, it's the identifier of the return entity
+    OmitType(CreateReturnPurchaseDto, ['purchaseRequestId'] as const) 
+) {}
