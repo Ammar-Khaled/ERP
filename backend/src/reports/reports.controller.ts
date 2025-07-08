a7ad900eb32d4117eaa182d4fb56a1027ba1055a
Ammar_Khaled <ammarkhaled2003@gmail.com>
2025-07-09 01:31:50 +0300

    feat: refactor report endpoints to use user token for getting branchId instead of url query string

--- backend/src/reports/reports.controller.ts
+++ backend/src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Request } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
@@ -7,55 +7,58 @@ export class ReportsController {
 
   // Get Total Orders Grouped By Day with Branch Filter
   @Get('orders/daily')
-  getDailyOrders(@Query('branchId') branchId?: number) {
-    return this.reportsService.getOrdersGroupedBy('day', branchId);
+  getDailyOrders(@Request() req: any) {
+    return this.reportsService.getOrdersGroupedBy('day', req.user.branchId);
   }
 
   // Get Total Orders Grouped By Month with Branch Filter
   @Get('orders/monthly')
-  getMonthlyOrders(@Query('branchId') branchId?: number) {
-    return this.reportsService.getOrdersGroupedBy('month', branchId);
+  getMonthlyOrders(@Request() req: any) {
+    return this.reportsService.getOrdersGroupedBy('month', req.user.branchId);
   }
 
   // Get Total Purchases Grouped By Day with Branch Filter
   @Get('purchases/daily')
-  getDailyPurchases(@Query('branchId') branchId?: number) {
-    return this.reportsService.getPurchasesGroupedBy('day', branchId);
+  getDailyPurchases(@Request() req: any) {
+    return this.reportsService.getPurchasesGroupedBy('day', req.user.branchId);
   }
 
   // Get Total Purchases Grouped By Month with Branch Filter
   @Get('purchases/monthly')
-  getMonthlyPurchases(@Query('branchId') branchId?: number) {
-    return this.reportsService.getPurchasesGroupedBy('month', branchId);
+  getMonthlyPurchases(@Request() req: any) {
+    return this.reportsService.getPurchasesGroupedBy(
+      'month',
+      req.user.branchId,
+    );
   }
 
   // Get Top Clients (Grouped by Month) and Metric (totalAmount | count) with Branch Filter
   @Get('top-clients')
   getTopClients(
     @Query('metric') metric: 'totalAmount' | 'count' = 'totalAmount',
-    @Query('branchId') branchId?: number,
+    @Request() req: any,
   ) {
-    return this.reportsService.getTopClients(metric, branchId);
+    return this.reportsService.getTopClients(metric, req.user.branchId);
   }
 
   // Get Top Suppliers (Grouped by Month) with Branch Filter
   @Get('top-suppliers')
-  getTopSuppliers(@Query('branchId') branchId?: number) {
-    return this.reportsService.getTopSuppliers(branchId);
+  getTopSuppliers(@Request() req: any) {
+    return this.reportsService.getTopSuppliers(req.user.branchId);
   }
 
   // Get Monthly Revenue with Branch Filter and Adjusted for Returns
   @Get('revenue/monthly')
-  getMonthlyRevenue(@Query('branchId') branchId?: number) {
-    return this.reportsService.getRevenueGroupedByMonth(branchId);
+  getMonthlyRevenue(@Request() req: any) {
+    return this.reportsService.getRevenueGroupedByMonth(req.user.branchId);
   }
 
   // Get Top Products Sold (Grouped by Month) with Branch Filter and Metric (quantity | revenue)
   @Get('top-products')
   getTopProducts(
     @Query('metric') metric: 'quantity' | 'revenue' = 'quantity',
-    @Query('branchId') branchId?: number,
+    @Request() req: any,
   ) {
-    return this.reportsService.getTopSoldProducts(metric, branchId);
+    return this.reportsService.getTopSoldProducts(metric, req.user.branchId);
   }
 }
