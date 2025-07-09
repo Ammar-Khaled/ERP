19901c6edcad4f6b3c5d083bce809686a7a2425e
Ammar_Khaled <ammarkhaled2003@gmail.com>
2025-07-09 18:52:58 +0300

    refactor: comment out seeding logic in onModuleInit for clarity

--- backend/src/permissions/permission.service.ts
+++ backend/src/permissions/permission.service.ts
@@ -26,15 +26,14 @@ export class PermissionService implements OnModuleInit {
   ) {}
 
   async onModuleInit() {
-    await this.permissionsSeeder.seed();
-
-    // Seed roles after permissions are created
-    const rolesSeeder = new (await import('../roles/roles.seeder')).RolesSeeder(
-      this.roleRepository,
-      this.permissionRepository,
-    );
-    await rolesSeeder.seed();
-
+    // await this.permissionsSeeder.seed();
+    //
+    // // Seed roles after permissions are created
+    // const rolesSeeder = new (await import('../roles/roles.seeder')).RolesSeeder(
+    //   this.roleRepository,
+    //   this.permissionRepository,
+    // );
+    // await rolesSeeder.seed();
     // Seed the admin user after roles are created
     // await this.userRepository.save({
     //   username: 'admin',
